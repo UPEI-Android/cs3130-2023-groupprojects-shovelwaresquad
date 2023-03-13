2d08a53bc7363ab679a372a17be05ea82e5e4c30
Juan Carlos Narvaez <jcnarvaeziturbe@hotmail.com>
2023-03-13 15:50:50 -0300

    Third Draft, added the UI for the popups for adding and removing an item from the list

--- Grocery_Keeper/lib/grocery/view/gkeeper_list.dart
+++ Grocery_Keeper/lib/grocery/view/gkeeper_list.dart
@@ -24,27 +24,116 @@ class _GKeeperList extends State<GKeeperList>{
       ),
       //TODO BlocBuilder in the body to use bloc API
       body: ListView.builder(
+        padding: const EdgeInsets.all(8),
         itemCount: temporaryList.length,
         itemBuilder: (BuildContext context, int index){
-          return Container(//Temporary until Tile is implemented
-            //TODO Implement Tile.dart
+          return Container(
+           height: 50,
+            child: Text(temporaryList[index]),
           );
         },
       ),
+      //TODO Navigation
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children:  <Widget>[
           FloatingActionButton(
-            onPressed: (){},
-            backgroundColor: Colors.lightBlueAccent,
-            heroTag: 'buttonRemove',
-            child: const Icon(Icons.remove),
-          ),
-          FloatingActionButton(
-            onPressed: (){},
+            onPressed: (){
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context){
+                    return AlertDialog(
+                      actionsAlignment: MainAxisAlignment.center,
+                      contentPadding: const EdgeInsets.only(
+                          top: 10.0
+                      ),
+                      title: const Text(
+                        "Add Item",
+                        textAlign: TextAlign.center,
+                      ),
+                      content: Container(
+                        height: 225,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Enter the item to add here',
+                                  labelText: 'Item',
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: 60,
+                              padding: const EdgeInsets.all(8),
+                              child: ElevatedButton(
+                                onPressed: (){},
+                                child: const Text('Search'),
+                              ),
+                            ),
+                            Container(
+                              height: 60,
+                              padding: const EdgeInsets.all(8),
+                              child: ElevatedButton(
+                                onPressed: (){},
+                                child: const Text('Add'),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }
+              );
+            },
             backgroundColor: Colors.lightBlueAccent,
             heroTag: 'buttonAdd',
             child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: (){
+              showDialog(
+                context: context,
+                builder: (BuildContext context){
+                  return AlertDialog(
+                    actionsAlignment: MainAxisAlignment.center,
+                    contentPadding: const EdgeInsets.only(
+                      top: 10.0
+                    ),
+                    title: const Text(
+                      'Remove Item',
+                      textAlign: TextAlign.center,
+                    ),
+                    content: Container(
+                      height: 300,
+                      width: 300,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                          itemCount: temporaryList.length,
+                          itemBuilder: (BuildContext context, int index){
+                            return Card(
+                              clipBehavior: Clip.hardEdge,
+                              child: ListTile(
+                                title: Text(temporaryList[index]),
+                                onTap: (){},
+                              ),
+                            );
+                          }
+                      ),
+                    )
+
+                  );
+                }
+
+              );
+            },
+            backgroundColor: Colors.lightBlueAccent,
+            heroTag: 'buttonRemove',
+            child: const Icon(Icons.remove),
           ),
         ],
       ),
